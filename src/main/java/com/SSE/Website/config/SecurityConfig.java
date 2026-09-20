f7bbe2083497978e127a87d27c8c3a4f4825e1dc
sandeepsinghpanwar1810 <sandeepsinghpanwar312@gmail.com>
2026-09-21 03:40:36 +0530

    Fix CORS configuration

--- src/main/java/com/SSE/Website/config/SecurityConfig.java
+++ src/main/java/com/SSE/Website/config/SecurityConfig.java
@@ -3,14 +3,11 @@ package com.SSE.Website.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,17 +31,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
 
-        http
-
-            // Disable CSRF because we are using JWT
-            .csrf(csrf -> csrf.disable())
-
-            // Stateless JWT authentication
-            .sessionManagement(session ->
-                session.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS
-                )
+    	http
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> {})
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
             )
+        )
 
             .authorizeHttpRequests(auth -> auth
 
@@ -56,6 +50,11 @@ public class SecurityConfig {
                     "/api/auth/verify-otp",
                     "/api/users/register"
                 ).permitAll()
+
+                // =========================
+                // PUBLIC ROOT URL
+                // =========================
+                .requestMatchers("/").permitAll()
 
                 // =========================
                 // PUBLIC PRODUCT IMAGES
